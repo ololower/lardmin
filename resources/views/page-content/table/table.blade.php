b83388f5b7355bda2f1be8716a7edded1c76c807
Anatolii Koziura <anatolii.koziura@gmail.com>
2020-12-16 01:49:12 +0200

    Добавлена возможность пагинации для выводимых таблиц

--- resources/views/page-content/table/table.blade.php
+++ resources/views/page-content/table/table.blade.php
@@ -1,37 +1,42 @@
-
-        <div class="align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            @if($rows->isEmpty())
-                <div class="text-center text-5xl font-black	text-gray-300 p-24">
-                    А здесь пусто :(
-                </div>
-            @else
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            @foreach($heading_row as $heading_row_item)
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                {{ $heading_row_item }}
-                            </th>
-                            @endforeach
-                        </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-
-                        @foreach($rows as $row)
-                            <tr>
-                                @foreach($row as $cell)
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="text-sm text-gray-900">{{ $cell }}</div>
-                                    </td>
-                                @endforeach
-                            </tr>
-                        @endforeach
-                        <!-- More rows... -->
-                        </tbody>
-                    </table>
-                </div>
-            @endif
-
-
+<div class="align-middle inline-block min-w-full sm:px-6 lg:px-8">
+    @if($rows->isEmpty())
+        <div class="text-center text-5xl font-black	text-gray-300 p-24">
+            {{ $empty_text }}
         </div>
+    @else
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                <tr>
+                    @foreach($heading_row as $heading_row_item)
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        {{ $heading_row_item }}
+                    </th>
+                    @endforeach
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+
+                @foreach($rows as $row)
+                    <tr>
+                        @foreach($row as $cell)
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{ $cell }}</div>
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+                <!-- More rows... -->
+                </tbody>
+            </table>
+        </div>
+
+        @if($pagination)
+        <div class="pagination pt-4">
+            {!! $pagination !!}
+        </div>
+        @endif
+    @endif
+
+
+</div>
--- src/ContentBuilder/Elements/Table/Table.php
+++ src/ContentBuilder/Elements/Table/Table.php
@@ -9,11 +9,16 @@ class Table extends FlatContentElement {
      * Table constructor.
      * @param array $heading_row элементы массива служат заголовками
      * @param array $rows контент таблицы
-     * @param string $text_empty текст, если контент пуст
+     * @param string|null $text_empty текст, если контент пуст
      */
-    public function __construct(array $heading_row, array $rows, $text_empty = '') {
+    public function __construct(array $heading_row, array $rows, $text_empty = null) {
         $this->view_params['heading_row'] = $heading_row;
         $this->view_params['rows'] = collect($rows);
+        $this->view_params['empty_text'] = $text_empty ?? "Записей не найдено!";
+    }
+
+    public function setPagination($pagination) {
+        $this->view_params['pagination'] = $pagination;
     }
 
     protected $view_name = "lardmin::page-content.table.table";
