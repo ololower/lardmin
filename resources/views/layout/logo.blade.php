71e309e14212270afe2fbfc78fab964713eef1e2
Anatolii Koziura <anatolii.koziura@gmail.com>
2020-12-18 02:50:10 +0200

    Место под логотип

--- resources/views/layout/logo.blade.php
+++ resources/views/layout/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full h-20 border-b flex px-4 items-center mb-8">
     <p class="font-semibold text-3xl text-blue-900 pl-4">
-        LARDMIN
+        <img src="http://placehold.it/200x60" alt="">
     </p>
 </div>
