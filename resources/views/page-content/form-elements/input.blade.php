0c0917fb8233979ae9f5a1e9beeccacb50ac22a9
Anatolii Koziura <anatolii.koziura@gmail.com>
2020-12-31 02:17:06 +0200

    Fix name property in input field

--- resources/views/page-content/form-elements/input.blade.php
+++ resources/views/page-content/form-elements/input.blade.php
@@ -3,6 +3,7 @@
     <input class='bg-white h-12 w-full px-3 rounded border shadow-inner text-sm focus:outline-none focus:border-gray-400'
            id='{{ $id }}'
            type='{{ $type }}'
+           name="{{ $name }}"
            placeholder='{{ $placeholder }}'
            value="{{ $value }}"
            @if ($readonly) readonly="true" disabled @endif
